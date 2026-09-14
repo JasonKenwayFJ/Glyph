967738970cb774f606919959dbe5275f3dde94d8
jason <jasonkenway.fj@gmail.com>
2026-09-14 17:29:59 +0300

    Refactor entity service functions for improved readability; update path definitions and format async function signatures

--- src/screens/components/EntityPage/TaskContent.tsx
+++ src/screens/components/EntityPage/TaskContent.tsx
@@ -1,9 +1,9 @@
 import {EntityPageContentProp} from "../../../types/LocalProps.ts";
-import {Searcher} from "../Shared/Searcher.tsx";
+
 import {Task} from "../Shared/Task/Task.tsx";
 
 export const TaskContent = (props: EntityPageContentProp) => {
-    let value: {} = "";
+
     return (
         <div className={"TasksContainer"}>
             <div style={{justifySelf: "center"}}>
